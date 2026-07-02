1c2579c39a9dbf877f40a23ed4f78ac0733c0dd0
rumarvez <109396096+rumarvez@users.noreply.github.com>
2026-07-02 20:43:38 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
+
+# এরপর আপনার বর্তমান কোডগুলো থাকবে (অর্থাৎ ২ নম্বর লাইন থেকে যা ছিল)
 import asyncio
 import io
 import re
